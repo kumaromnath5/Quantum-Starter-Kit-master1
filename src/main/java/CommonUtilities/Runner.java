65acc41237be1924ac104663e9873507a6a2bf7f
kumaromnath5 <37383320+kumaromnath5@users.noreply.github.com>
2018-03-26 22:57:03 +0530

    commiting for new amozon automated page

--- src/main/java/CommonUtilities/Runner.java
+++ src/main/java/CommonUtilities/Runner.java
@@ -5,9 +5,7 @@ import org.testng.annotations.Test;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "scenarios", glue = "com.quantum.steps", tags ={"@navigate_to_flipkart_home_page"}, dryRun = false, strict = false)
-
-
+@CucumberOptions(features = "scenarios", glue = "com.quantum.steps", tags ={"@amazonpage"}, dryRun = false, strict = false)
 
 public class Runner extends AbstractTestNGCucumberTests {
 	@Test
