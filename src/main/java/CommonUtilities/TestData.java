f00c4587bf13ae0f5bb097847e5ff16cf77378b3
kumaromnath5 <37383320+kumaromnath5@users.noreply.github.com>
2018-04-09 20:24:18 +0530

    testdata

---
+++ src/main/java/CommonUtilities/TestData.java
@@ -0,0 +1,17 @@
+package CommonUtilities;
+
+public class TestData {
+
+	// UAT environment URL
+	
+	public static final String UAT_URL="https://www.hihonor.com/in/index.html";
+	
+	//Test Environment URL
+	
+	public static final String TEST_URL="https://www.hihonor.com/in/index.html";
+	
+	
+	//Prod Environment URL
+	
+	public static final String PROD_URL="https://www.hihonor.com/in/index.html";
+}
